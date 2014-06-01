444c4fd3ef330a9076752e6d7d2cfd4e31ec33a6
t6siddiq <tawfiq.a.siddiqui@gmail.com>
2014-06-01 14:22:31 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+function clickHighlight(){
+ 	document.getElementById('highlights').scrollIntoView();	
+}
+function clickExp(){
+ 	document.getElementById('exp').scrollIntoView();	
+}
+
+function clickSkill(){
+ 	document.getElementById('skill').scrollIntoView();	
+}
+
+function clickEdu(){
+ 	document.getElementById('edu').scrollIntoView();	
+}
+function clickPro(){
+ 	document.getElementById('pro').scrollIntoView();	
+}
+function clickCourse(){
+ 	document.getElementById('course').scrollIntoView();	
+}
+function clickTop(){
+ 	document.getElementById('top').scrollIntoView();	
+}
+
